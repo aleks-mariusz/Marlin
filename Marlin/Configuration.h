a7711a0ee70c353a5486212a48fce3f9c05fcf35
aleks-mariusz <a+github@alek.cx>
2020-03-18 17:45:23 +0000

    re-set bed size and min/max for x/y with new heat insulation pad

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -887,15 +887,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
+#define X_BED_SIZE 215
 #define Y_BED_SIZE 275
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -1
+#define X_MIN_POS 0
 #define Y_MIN_POS -1
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE + 6
-#define Y_MAX_POS Y_BED_SIZE + 4
+#define X_MAX_POS X_BED_SIZE + 10
+#define Y_MAX_POS Y_BED_SIZE + Y_BED_SIZE
 #define Z_MAX_POS 195
 
 /**
