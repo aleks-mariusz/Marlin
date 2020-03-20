1dee27e650b485c2c397c997c64ef47544075586
aleks-mariusz <a+github@alek.cx>
2020-03-20 02:59:47 +0000

    re-set bed size, min/max for x/y after y-rails moved to maximize bed size

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -887,15 +887,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 215
-#define Y_BED_SIZE 275
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 272
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS -1
+#define X_MIN_POS -1
+#define Y_MIN_POS -2
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE + 10
-#define Y_MAX_POS Y_BED_SIZE + Y_BED_SIZE
+#define X_MAX_POS X_MIN_POS + X_BED_SIZE + 5
+#define Y_MAX_POS Y_MIN_POS + Y_BED_SIZE
 #define Z_MAX_POS 195
 
 /**
